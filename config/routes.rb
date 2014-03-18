3c42a48677fd65d779260d555f04523cb591228a
Zach Feldman <zachfeldman@gmail.com>
2014-03-18 14:07:47 -0400

    OmniAuth + Devise + GitHub.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Deviser::Application.routes.draw do
+  get "users/index"
   root to: "home#index"
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
